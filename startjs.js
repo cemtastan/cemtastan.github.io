c51bd0822cdb8ea99b07cda5313aefc3dc2709d4
cemtastan <cemtastan1234@gmail.com>
2020-07-30 00:20:06 +0300

    changed the content

---
+++ startjs.js
@@ -0,0 +1,9 @@
+function changeContent() {
+    document.getElementById("testcontent").innerHTML = `
+    
+    <div class="bottom-text col-lg-9 col-sm-12">
+    <p>Data analysis is a process of inspecting, cleansing, transforming and modeling data with the goal of discovering useful information, informing conclusions and supporting decision-making. Data analysis has multiple facets and approaches, encompassing diverse techniques under a variety of names, and is used in different business, science, and social science domains. In today's business world, data analysis plays a role in making decisions more scientific and helping businesses operate more effectively.</p>
+</div>
+    `;
+
+}
