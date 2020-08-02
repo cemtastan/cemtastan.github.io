54b967cfecc6a534bbd0692ac3ae2eb5680bfeeb
cemtastan <cemtastan1234@gmail.com>
2020-08-02 14:00:12 +0300

    menu ve navigator gücellemesi

--- startjs.js
+++ startjs.js
@@ -1,9 +1,29 @@
-function changeContent() {
+function changeHome() {
     document.getElementById("testcontent").innerHTML = `
     
     <div class="bottom-text col-lg-9 col-sm-12">
     <p>Data analysis is a process of inspecting, cleansing, transforming and modeling data with the goal of discovering useful information, informing conclusions and supporting decision-making. Data analysis has multiple facets and approaches, encompassing diverse techniques under a variety of names, and is used in different business, science, and social science domains. In today's business world, data analysis plays a role in making decisions more scientific and helping businesses operate more effectively.</p>
 </div>
     `;
+       
+}
 
+
+function changeAboutus() {
+    document.getElementById("testcontent").innerHTML = `
+    
+    <div class="middle-content-text col-lg-9 col-sm-12">                  
+                    <p >A database is an organized collection of data, generally stored and accessed electronically from a computer system. Where databases are more complex they are often developed using formal design and modeling techniques. The database management system (DBMS) is the software that interacts with end users, applications, and the database itself to capture and analyze the data. The DBMS software additionally encompasses the core facilities provided to administer the database. The sum total of the database, the DBMS and the associated applications can be referred to as a "database system". Often the term "database" is also used to loosely refer to any of the DBMS, the database system or an application associated with the database.</p>
+                </div>
+                `;
+}
+
+function changeContent() {
+    document.getElementById("testcontent").innerHTML = `
+    
+    <div class="header-text col-lg-8 col-sm-12">
+                <p style = "font-family:courier,arial,helvetica;">
+                    A computer programmer,sometimes called a software developer, a programmer or more recently a coder (especially in more informal contexts), is a person who creates computer software. The term computer programmer can refer to a specialist in one area of computers, or to a generalist who writes code for many kinds of software.</p>
+            </div>
+                `;
 }
